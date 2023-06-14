69e1138f2b10547fa1338704778d4f167b52905a
Jeremias-ai-coder <jeremiascoll@gmail.com>
2023-06-14 19:08:39 -0300

    Agregamos la nueva animación

--- src/Scene.ts
+++ src/Scene.ts
@@ -1,16 +1,38 @@
-import { Container } from "pixi.js";
+import { AnimatedSprite, Container, Texture } from "pixi.js";
 import { TijerasYlapiz } from "./TijerasYlapiz";
 
 export class Scene extends Container{
 
     constructor(){
         super();
+        
         const tijerasYlapiz:TijerasYlapiz= new TijerasYlapiz();
-
         tijerasYlapiz.scale.set(0.5);
         tijerasYlapiz.x=200;
         tijerasYlapiz.y=300;
-    
-       this.addChild(tijerasYlapiz);
+        this.addChild(tijerasYlapiz);
+
+       const FightAnimated: AnimatedSprite = new AnimatedSprite(
+         [
+            Texture.from("fight1"),
+            Texture.from("fight2"),
+            Texture.from("fight3"),
+            Texture.from("fight4"),
+            Texture.from("fight5"),
+            Texture.from("fight6"),
+            Texture.from("fight7"),
+            Texture.from("fight6"),
+            Texture.from("fight5"),
+            Texture.from("fight4"),
+            Texture.from("fight3"),
+            Texture.from("fight2"),
+            Texture.from("fight1")
+         ],
+         true
+       );
+       FightAnimated.play();
+       FightAnimated.animationSpeed = 0.2;
+       FightAnimated.scale.set(0.1);
+       this.addChild(FightAnimated);
     }
 }
--- src/assets.ts
+++ src/assets.ts
@@ -3,6 +3,14 @@
 export const assets = [
 
     {url: "./tijeras.png", name: "nuestraTijera"},
-    {url: "./lapiz.png",name:"lapiz"}
+    {url: "./lapiz.png",name:"lapiz"},
+
+    {url: "./fight/fighting1.png", name: "fight1"},
+    {url: "./fight/fighting2.png", name: "fight2"},
+    {url: "./fight/fighting3.png", name: "fight3"},
+    {url: "./fight/fighting4.png", name: "fight4"},
+    {url: "./fight/fighting5.png", name: "fight5"},
+    {url: "./fight/fighting6.png", name: "fight6"},
+    {url: "./fight/fighting7.png", name: "fight7"}
 
 ];
